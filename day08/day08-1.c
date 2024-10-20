67c8d4ccf1de4de96919fa6a0ff6ff24e729ac90
Tear020 <jcc58919411@gmail.com>
2024-10-20 15:09:44 +0900

    Update day08-1.c

--- day08/day08-1.c
+++ day08/day08-1.c
@@ -6,12 +6,9 @@ int main(void)
 	char str[100];
 	char *temp;
 	temp = str;
-	
+
 	printf("문자열을 입력하세요 : ");
 	scanf_s("%s", str, sizeof(str));
-
-
-	
 
 	for (int i = 0; i < strlen(str) / 2; i++)
 	{
@@ -19,6 +16,5 @@ int main(void)
 		str[strlen(str) - (i + 1)] = str[i];
 		str[i] = temp;
 	}
-
 	printf("%s", str);
 }
