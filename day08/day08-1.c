206ba9e6cac9eb8f98d681cd8a95920192b67ff9
Tear020 <jcc58919411@gmail.com>
2024-10-20 14:57:42 +0900

    문자열 뒤집기

---
+++ day08/day08-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str[100];
+	char *temp;
+	temp = str;
+	
+	printf("���ڿ��� �Է��ϼ��� : ");
+	scanf_s("%s", str, sizeof(str));
+
+	
+
+	for (int i = 0; i < strlen(str) / 2; i++)
+	{
+		temp = str[strlen(str) - (i + 1)];
+		str[strlen(str) - (i + 1)] = str[i];
+		str[i] = temp;
+	}
+
+	printf("%s", str);
+}
