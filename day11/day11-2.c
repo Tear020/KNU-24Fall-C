41dab29c93854975ba384286c008e0ec3c56e7de
nanakyeong <129843360+nanakyeong@users.noreply.github.com>
2024-11-21 14:50:15 +0900

    Update day11-2.c

--- day11/day11-2.c
+++ day11/day11-2.c
@@ -6,16 +6,26 @@ int main(void)
 	char fx[100];
 	int a, b, c; //ax^2+bx+c
 	a = 1, b = 0, c = 1; // x^2+1
-	int start, end, range;
+	float start, end, n;
+	float base;
+	float y = 0;
 	printf("적분의 시작 값을 입력하세요(fx = x^2 + 1) : ");
-	scanf_s("%d", &start);
+	scanf_s("%f", &start);
 	printf("적분할 끝 값을 입력하세요 : ");
-	scanf_s("%d", &end);
+	scanf_s("%f", &end);
 	printf("시행할 최대 구간을 입력하세요(2^n) : ");
-	scanf_s("%d", &range);
+	scanf_s("%f", &n);
 
-	printf("밑변  : %d\n", end - start);
-	printf("x가 end 일 때 %d\n", (end * end * a) + (end * b) + c);
-	printf("x가 start 일 때 %d\n", (start * start * a) + (start * b) + c);
+	//base = (end - start) / n;
+
+	for (int i = 0; i <= (int)n; i++)
+	{
+		base = (end - start) / i;
+		(start  + base) 
+	}
+
+	/*printf("밑변  : %d\n", (end - start)/n);
+	printf("x가 end 일 때 %f\n", (end * end * a) + (end * b) + c);
+	printf("x가 start 일 때 %f\n", (start * start * a) + (start * b) + c);*/
 
 }
